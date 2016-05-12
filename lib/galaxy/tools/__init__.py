3ab8e7742931007644956e1923c39248137ef35f
Christophe Antoniewski <drosofff@gmail.com>
2016-05-12 10:08:04 +0200

    Revert "edit lib/galaxy/tools/__init__.py as proposed by mvdbeek (https://github.com/galaxyproject/galaxy/pull/2337/files)"
    a better fix in PR #2345
    This reverts commit 4127ec0fdd426fc152e4af9c19b1c46ac93a3b2a.

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -1919,7 +1919,6 @@ class OutputParameterJSONTool( Tool ):
     def _prepare_json_param_dict( self, param_dict ):
         rval = {}
         for key, value in param_dict.iteritems():
-            key = str(key)
             if isinstance( value, dict ):
                 rval[ key ] = self._prepare_json_param_dict( value )
             elif isinstance( value, list ):
